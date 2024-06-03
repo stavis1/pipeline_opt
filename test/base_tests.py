da2256f95f5d07bdfdf1eb57c01de326805ee625
anon <stavis@vols.utk.edu>
2024-06-03 13:11:30 -0400

    set up initial testing infrastructure

---
+++ test/base_tests.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  3 12:58:11 2024
+
+@author: anon
+"""
+
---
+++ test/run_tests.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import unittest
+from options_tests import *
+from worker_tests import *
+from scheduler_tests import *
+from support_tests import *
+
+if __name__ == '__main__':
+    unittest.main()
+
